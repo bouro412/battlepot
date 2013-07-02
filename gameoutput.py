78304c6ef7ab312078eab241b132f511d525eb66
flying-polyp <keita.bouro@gmail.com>
2013-07-02 23:19:31 +0900

    color test

--- gameoutput.py
+++ gameoutput.py
@@ -5,67 +5,85 @@ from math import *
 x=0.0
 y=0.0
 angle=0.0
-
-def draw(ws_states,ad_states):
+def camera(ws_states, ad_states):
      global x,y,angle
      if ws_states != 0:
           x += cos(radians(angle)) * ws_states * 0.5
           y += -sin(radians(angle)) * ws_states * 0.5
      if ad_states != 0:
-          angle += ad_states * 3
-
-     glClearColor(0,1,1,1)
+          angle += ad_states * 3     
      glLoadIdentity()
      camera_position = [x -10 * cos(radians(angle))
-                        ,1,y + 10 * sin(radians(angle))]
+                        ,2,y + 10 * sin(radians(angle))]
      camera_vector = [x - camera_position[0],0,y - camera_position[2]]
      gluLookAt(camera_position[0],camera_position[1],camera_position[2],
                camera_position[0] + camera_vector[0] * 10,1,
                camera_position[2] + camera_vector[2] * 10,
                0,1,0)
+
+def draw(ws_states,ad_states):
+     glClearColor(0,1,1,1)
      glClearDepth(1)
      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
      glEnable(GL_DEPTH_TEST)
+
+     camera(ws_states, ad_states)
      light()
-     teapot()
+
      drawxyz()
-     """glPushMatrix()
-     glRotatef(75,0,1,0)
-     glTranslatef(1,0,0)
-     drawtri()
-     glPopMatrix()
-
-     glPushMatrix()
-     glTranslatef(1,0,0)
-
-
-     glRotatef(75,0,1,0)
-     drawtri()
-     glPopMatrix()
-
-     drawxyz()"""
-     
+     drawplayer()
+     #drawearth()
+        
      glDisable(GL_DEPTH_TEST)
      glutSwapBuffers()
     
 def drawxyz():
-    glBegin(GL_LINES)
-    glColor(0,0,0)
-    glVertex3f(0,0,0)
-    glVertex3f(100,0,0)
-    glVertex3f(0,0,0)
-    glVertex3f(0,100,0)
-    glVertex3f(0,0,0)
-    glVertex3f(0,0,100)
-    glEnd()
+     white = [1,1,1,1]
+     glPushMatrix()
+     glBegin(GL_LINES)
+     glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT, white)
+     glMaterialfv(GL_FRONT_AND_BACK,GL_DIFFUSE, white)
+     glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR, white)
+     glVertex3f(0,0,0)
+     glVertex3f(100,0,0)
+     glVertex3f(0,0,0)
+     glVertex3f(0,100,0)
+     glVertex3f(0,0,0)
+     glVertex3f(0,0,100)
+     glEnd()
+     glPopMatrix()
 
 def drawearth():
+     red = [1,0,0,1]
+     green = [0,1,0,1]
+     glPushMatrix()
      glBegin(GL_QUADS)
-     glVertex3f(-100,-1.5,-100)
-     glVertex3f(-100,-1.5,100)
-     glVertex3f(100,-1.5,100)
-     glVertex3f(100,-1.5,-100)
+     for i in range(100):
+          for j in range(100):
+               glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT, red)
+               glMaterialfv(GL_FRONT_AND_BACK,GL_DIFFUSE, red)
+               glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR, red)
+               glVertex3f(i,0,j)
+               glVertex3f(i+0.5,0,j)
+               glVertex3f(i+0.5,0,j+0.5)
+               glVertex3f(i,0,j+0.5)
+               glVertex3f(i,0,j)
+               glVertex3f(i-0.5,0,j)
+               glVertex3f(i-0.5,0,j-0.5)
+               glVertex3f(i,0,j-0.5)
+               glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT,green)
+               glMaterialfv(GL_FRONT_AND_BACK,GL_DIFFUSE,green)
+               glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,green)
+               glVertex3f(i,0,j)
+               glVertex3f(i+0.5,0,j)
+               glVertex3f(i+0.5,0,j-0.5)
+               glVertex3f(i,0,j-0.5)
+               glVertex3f(i,0,j)
+               glVertex3f(i-0.5,0,j)
+               glVertex3f(i-0.5,0,j+0.5)
+               glVertex3f(i,0,j+0.5)
      glEnd()
+     glPopMatrix()
 
 def reshape(w,h):
      glViewport(0,0,w,h)
@@ -92,28 +110,25 @@ def light():
 
     glLightfv(GL_LIGHT0, GL_POSITION, lightpos)
 
-def teapot():
+def drawplayer():
      global x,y,angle
+     glPushMatrix()
+
+     glTranslatef(x,0.7,y)
+     glRotatef(angle,0,1,0)
+     golden_pot()
+ 
+     glPopMatrix()
+
+def golden_pot():
      gold_amb = [0.247250, 0.1995, 0.07450, 1.0]
      gold_diff = [0.75164, 0.60648, 0.22648, 1.0]
      gold_spe = [0.628281, 0.555802, 0.366065, 1.0]
      gold_shin = 51.2
-     glPushMatrix()
+     
      glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, gold_amb)    
      glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, gold_diff)
      glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, gold_spe)
      glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, gold_shin)
-     """glColorMaterial(GL_FRONT_AND_BACK,GL_AMBIENT)
-     glColor4fv(gold_amb)
-     glColorMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE)
-     glColor4fv(gold_diff)
-     glColorMaterial(GL_FRONT_AND_BACK, GL_SPECULAR)
-     glColor4fv(gold_spe)"""
-     glTranslatef(x,0.0,y)
-     glRotatef(angle,0,1,0)
+
      glutSolidTeapot(1.0)
-     #glDisable(GL_COLOR_MATERIAL)
-     glPopMatrix()
-
-
-     
