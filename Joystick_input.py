44e7b5aa00e87ac64cdc5a062d4785176d6e53eb
flying-polyp <keita.bouro@gmail.com>
2013-07-11 20:37:54 +0900

    joyinput class

--- Joystick_input.py
+++ Joystick_input.py
@@ -1,37 +1,53 @@
 import time
 import pygame
 
-def getxbox360pad():
-    for i in range( pygame.joystick.get_count() ):
-        js = pygame.joystick.Joystick( i )
-        if js.get_name() == 'Microsoft X-Box 360 pad':
-            return js
-    raise RuntimeError( 'X-Box 360 pad not found' )
+class joyinput:
+    def __init__(self,axis=[0,0,-1,0,0,-1],
+                 buttons=[False for i in range(10)]):
+        self.axis = axis
+        self.buttons = buttons
+        self.js = None
 
-def init():
-    global js
-    pygame.init()
-    pygame.joystick.init()
-    js = getxbox360pad()
-    js.init()
+    def getxbox360pad(self):
+        for i in range( pygame.joystick.get_count() ):
+            js = pygame.joystick.Joystick( i )
+            if js.get_name() == 'Microsoft X-Box 360 pad':
+                return js
+        raise RuntimeError( 'X-Box 360 pad not found' )
 
-def quit():
-    js.quit()
-    pygame.quit()
+    def init(self):
+        pygame.init()
+        pygame.joystick.init()
+        self.js = self.getxbox360pad()
+        self.js.init()
+    
+    def quit(self):
+        self.js.quit()
+        pygame.quit()
+    
+    def input():
+        e = pygame.event.get()
+        for i in e:
+            if i.type == pygame.JOYAXISMOTION:
+                self.axis[i.axis] = i.value
+            elif i.type == pygame.JOYBUTTONDOWN:
+                self.button[i.button] = True
+            elif i.type == pygame.JOYHATMOTION:
+                pass
 
 def test():
     while True:
         try:
             e = pygame.event.wait()
-            print e.type()
-            if e.value >= 0.7:
-                print( e )
+            print( e )
+            print e.type
         except KeyboardInterrupt:
-            system.exit(1)
+            exit()
 if __name__=="__main__":
     try:
-        init()
+        pad = joyinput()
+        pad.init()
         test()
     finally:
-        quit()
+        pad.quit()
 
